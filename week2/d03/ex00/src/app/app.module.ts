447319d6b9c69631d57a7f5ab5c0645847eb3860
Alessandro Fraccalvieri <afraccal@e3r3p4.42roma.it>
2023-07-27 16:44:00 +0200

    d03

---
+++ week2/d03/ex00/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+import { ParentComponent } from './parent/parent.component';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    TableComponent,
+    ParentComponent,
+    NavComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week2/d03/ex01/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
+import { ParentComponent } from './parent/parent.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    FormComponent,
+    ParentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
